1b036cab19dbcd4188c16a99f1d4b9adad98c601
Valentin <valentin@molina.pro>
2021-12-02 09:26:26 +0100

    Setup maven project

---
+++ image_pgm/src/main/java/org/centrale/image_pgm/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.image_pgm;
+
+/**
+ *
+ * @author Valentin Molina valentin@molina.pro
+ */
+public class Main {
+    
+    public static void main(String[] argv)
+    {
+        System.out.println("Hello");
+    }
+    
+}
